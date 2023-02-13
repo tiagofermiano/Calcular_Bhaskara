a4395638ff3eac8375d168110ca49c247db2517e
medeiros-augusto <augustomedeiros047@gmail.com>
2023-02-13 00:15:22 -0300

    Importando primeiro código

---
+++ bhaskrara.py
@@ -0,0 +1,22 @@
+import math
+
+#Recebendo os valores de A, B e C
+
+a = int(input("Digite o valor de A: "))
+b = int(input("Digite o valor de B: "))
+c = int(input("Digite o valor de C: "))
+
+#Calculando o Delta
+
+d = (b*b) -4*a*c
+
+#Condição e Calculando o X
+if d < 0:
+    print("A equação não possui raizes reais!")
+if d == 0:
+    x1 = ((-b) + math.sqrt(d)) / (2*a)
+    print("A equação possui uma raiz real {}!".format(x1))
+if d > 0:
+    x1 = ((-b) + math.sqrt(d)) / (2*a)
+    x2 = ((-b) - math.sqrt(d)) / (2*a)
+    print("A equação possui duas raizes reais {} e {}!".format(x1,x2))
